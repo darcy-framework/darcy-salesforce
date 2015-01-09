972ca957431fcdebb34eae344d340ca23b9432bc
Jimmy Ver Vaecke <jvervaec@redhat.com>
2015-01-09 17:27:17 -0500

    Add another BySalesforce locator and document the class

--- src/main/java/com/redhat/darcy/salesforce/BySalesforce.java
+++ src/main/java/com/redhat/darcy/salesforce/BySalesforce.java
@@ -27,24 +27,48 @@ import com.redhat.darcy.ui.api.elements.Findable;
 import java.util.List;
 import java.util.Objects;
 
-public abstract class BySalesforce {
-    public static BySalesforceLabel label(String subHeading, String label) {
-        return new BySalesforceLabel(subHeading, label);
-    }
+/**
+ * Locator convenience class for finding elements on Salesforce layouts using the text of their
+ * labels.  Due to variations in the Salesforce DOM (standard layout, standard layout in edit mode, Visualforce layout), 
+ * select a locator strategy based on the type of Salesforce page with which you are attempting to interact.
+ * 
+ * @author jvervaec
+ *
+ */
 
-    public static class BySalesforceLabel implements Locator {
+public abstract class BySalesforce {
+    public static BySalesforceStdLabel stdlabel(String subHeading, String label) {
+        return new BySalesforceStdLabel(subHeading, label);
+    }
+    
+    public static BySalesforceEditLabel editLabel(String subHeading, String label) {
+        return new BySalesforceEditLabel(subHeading, label);
+    }
+    
+    /**
+     * On a Standard Salesforce table (not in edit mode), locates a given (usually text or img) element by the preceding 
+     * text label.  This may work for Visualforce pages as well, but there is lots of variability in how they are laid out 
+     * in the apex code.
+     * 
+     * @author jvervaec
+     *
+     */
+    public static class BySalesforceStdLabel implements Locator {
         private final String subHeading;
         private final String label;
 
         /** The computed locator from the given subHeading and label. */
         private final Locator locator;
 
-        public BySalesforceLabel(String subHeading, String label) {
+        public BySalesforceStdLabel(String subHeading, String label) {
             this.subHeading = Objects.requireNonNull(subHeading, "subHeading");
             this.label = Objects.requireNonNull(label, "label");
 
-            String xpath = "//div[@class='pbSubsection']//td[" + withClass("dataCol") + " and " +
-                    "preceding-sibling::td[1][" + withClass("labelCol") + " and text()='" + label + "']]";
+            String xpath = "//div[@class='pbSubsection']//td[" + withClass("dataCol") + 
+                    " and " + "(preceding-sibling::td[1][" + withClass("labelCol") + 
+                    " and text()='" + label + "'] " + 
+                    "| preceding-sibling::td[1][" + withClass("labelCol") + 
+                    "]/span[text()='" + label + "'])]";
 
             locator = By.xpath(xpath);
         }
@@ -76,7 +100,7 @@ public abstract class BySalesforce {
                 return false;
             }
 
-            BySalesforceLabel that = (BySalesforceLabel) o;
+            BySalesforceStdLabel that = (BySalesforceStdLabel) o;
 
             return label.equals(that.label) && subHeading.equals(that.subHeading);
 
@@ -84,7 +108,7 @@ public abstract class BySalesforce {
 
         @Override
         public String toString() {
-            return "BySalesforceLabel: {" +
+            return "BySalesforceStdLabel: {" +
                     "subHeading='" + subHeading + '\'' +
                     ", label='" + label + '\'' +
                     ", locator=" + locator +
@@ -93,6 +117,72 @@ public abstract class BySalesforce {
 
         private String withClass(String className) {
             return "contains(concat(' ', normalize-space(@class), ' '), ' " + className + " ')";
+        }
+    }
+    
+    /**
+     * On a standard Salesforce page which is currently in edit mode, locates an html element by its preceding label 'for' 
+     * attribute.  This will likely not work for Visualforce pages because the DOM looks more like the standard page, even 
+     * though you have interactive html elements.
+     * 
+     * @author jvervaec
+     *
+     */
+    public static class BySalesforceEditLabel implements Locator {
+        private final String subHeading;
+        private final String label;
+
+        /** The computed locator from the given subHeading and label. */
+        private final Locator locator;
+
+        public BySalesforceEditLabel(String subHeading, String label) {
+            this.subHeading = Objects.requireNonNull(subHeading, "subHeading");
+            this.label = Objects.requireNonNull(label, "label");
+
+            
+            locator = By.xpath("//*[@id = //label[text() = '" + label + "']/@for]");
+        }
+
+        @Override
+        public <T extends Findable> List<T> findAll(Class<T> type,
+                Context context) {
+            return locator.findAll(type, context);
+        }
+
+        @Override
+        public <T extends Findable> T find(Class<T> type, Context context) {
+            return locator.find(type, context);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(subHeading, label);
+
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            BySalesforceEditLabel that = (BySalesforceEditLabel) o;
+
+            return label.equals(that.label) && subHeading.equals(that.subHeading);
+
+        }
+
+        @Override
+        public String toString() {
+            return "BySalesforceEditLabel: {" +
+                    "subHeading='" + subHeading + '\'' +
+                    ", label='" + label + '\'' +
+                    ", locator=" + locator +
+                    '}';
         }
     }
 }
